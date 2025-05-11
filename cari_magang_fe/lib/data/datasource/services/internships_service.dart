7379abb0c90d03d2beb00e25aee0a568d2cfcd7d
Mustafa Hadil <hadilulya21@gmail.com>
2025-05-11 13:39:30 +0700

    solve fix

--- cari_magang_fe/lib/data/datasource/services/internships_service.dart
+++ cari_magang_fe/lib/data/datasource/services/internships_service.dart
@@ -10,4 +10,22 @@ class InternshipsService {
       receiveTimeout: Duration(seconds: 5),
     ),
   );
+
+  Future<Either<String, InternshipsModel>> getInternships() async {
+    try {
+      var response = await dio.get('/internships');
+      var internships = InternshipsModel.fromMap(response.data);
+      return Right(internships);
+    } on DioException catch (e) {
+      if (e.response?.statusCode == 404) {
+        var errorResponse = e.response?.data['message'];
+        return Left('Error : $errorResponse');
+      } else if (e.response?.statusCode == 401) {
+        var errorResponse = e.response?.data['message'];
+        return Left('Error : $errorResponse');
+      } else {
+        return Left('Undhandle Error : ${e.message}');
+      }
+    }
+  }
 }
